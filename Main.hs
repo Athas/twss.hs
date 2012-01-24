5e3bc23d99d418c51a5d3e26beaed9989b272b0a
Troels Henriksen <athas@sigkill.dk>
2012-01-24 11:00:48 +0100

    Make filter program.

--- Main.hs
+++ Main.hs
@@ -2,13 +2,12 @@ module Main (main) where
 
 import Twss
 import System.Environment
-import System.Exit
 
 main :: IO ()
 main = do args <- getArgs
           case args of
-            [posf, negf, s] -> do
+            [posf, negf] -> do
                       pos <- readTrainingData posf
                       neg <- readTrainingData negf
-                      exitWith (if isTwss pos neg s then ExitSuccess else ExitFailure 1)
-            _ -> do error "usage: <positives-file> <negatives-file> string"
+                      interact $ unlines . filter (isTwss pos neg) . lines
+            _ -> do error "usage: <positives-file> <negatives-file>"
